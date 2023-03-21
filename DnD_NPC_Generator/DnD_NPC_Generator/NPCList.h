a0c34c5a11046784739b536ece33a5202bfc5643
Sebastià Ciudad Bosch <sebastia.ciudad@gmail.com>
2023-03-21 23:40:50 +0100

    Forms visuals
    I created the basic visual of each form

--- DnD_NPC_Generator/DnD_NPC_Generator/NPCList.h
+++ DnD_NPC_Generator/DnD_NPC_Generator/NPCList.h
@@ -34,7 +34,16 @@ namespace DnDNPCGenerator {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ lbl_Prueba;
+	private: System::Windows::Forms::Panel^ pnl_correccion;
+	private: System::Windows::Forms::Panel^ pnl_titulo;
+	private: System::Windows::Forms::Panel^ pnl_central;
+	private: System::Windows::Forms::Label^ lbl_titulo;
+	protected:
+
+	protected:
+
+
+
 	protected:
 
 	private:
@@ -50,29 +59,69 @@ namespace DnDNPCGenerator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->lbl_Prueba = (gcnew System::Windows::Forms::Label());
+			this->pnl_correccion = (gcnew System::Windows::Forms::Panel());
+			this->pnl_titulo = (gcnew System::Windows::Forms::Panel());
+			this->pnl_central = (gcnew System::Windows::Forms::Panel());
+			this->lbl_titulo = (gcnew System::Windows::Forms::Label());
+			this->pnl_titulo->SuspendLayout();
 			this->SuspendLayout();
 			// 
-			// lbl_Prueba
+			// pnl_correccion
 			// 
-			this->lbl_Prueba->AutoSize = true;
-			this->lbl_Prueba->Location = System::Drawing::Point(82, 124);
-			this->lbl_Prueba->Name = L"lbl_Prueba";
-			this->lbl_Prueba->Size = System::Drawing::Size(44, 13);
-			this->lbl_Prueba->TabIndex = 1;
-			this->lbl_Prueba->Text = L"NPC list";
+			this->pnl_correccion->Dock = System::Windows::Forms::DockStyle::Top;
+			this->pnl_correccion->Location = System::Drawing::Point(0, 0);
+			this->pnl_correccion->Name = L"pnl_correccion";
+			this->pnl_correccion->Size = System::Drawing::Size(1044, 30);
+			this->pnl_correccion->TabIndex = 0;
+			// 
+			// pnl_titulo
+			// 
+			this->pnl_titulo->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(46)), static_cast<System::Int32>(static_cast<System::Byte>(79)),
+				static_cast<System::Int32>(static_cast<System::Byte>(79)));
+			this->pnl_titulo->Controls->Add(this->lbl_titulo);
+			this->pnl_titulo->Dock = System::Windows::Forms::DockStyle::Top;
+			this->pnl_titulo->Location = System::Drawing::Point(0, 30);
+			this->pnl_titulo->Name = L"pnl_titulo";
+			this->pnl_titulo->Size = System::Drawing::Size(1044, 60);
+			this->pnl_titulo->TabIndex = 1;
+			// 
+			// pnl_central
+			// 
+			this->pnl_central->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(203)), static_cast<System::Int32>(static_cast<System::Byte>(228)),
+				static_cast<System::Int32>(static_cast<System::Byte>(222)));
+			this->pnl_central->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->pnl_central->Location = System::Drawing::Point(0, 90);
+			this->pnl_central->Name = L"pnl_central";
+			this->pnl_central->Size = System::Drawing::Size(1044, 571);
+			this->pnl_central->TabIndex = 2;
+			// 
+			// lbl_titulo
+			// 
+			this->lbl_titulo->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->lbl_titulo->Font = (gcnew System::Drawing::Font(L"Algerian", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lbl_titulo->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(203)), static_cast<System::Int32>(static_cast<System::Byte>(228)),
+				static_cast<System::Int32>(static_cast<System::Byte>(222)));
+			this->lbl_titulo->Location = System::Drawing::Point(0, 0);
+			this->lbl_titulo->Name = L"lbl_titulo";
+			this->lbl_titulo->Size = System::Drawing::Size(1044, 60);
+			this->lbl_titulo->TabIndex = 0;
+			this->lbl_titulo->Text = L"Generate NPC list";
+			this->lbl_titulo->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// NPCList
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1044, 661);
-			this->Controls->Add(this->lbl_Prueba);
+			this->Controls->Add(this->pnl_central);
+			this->Controls->Add(this->pnl_titulo);
+			this->Controls->Add(this->pnl_correccion);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"NPCList";
 			this->Text = L"NPCList";
+			this->pnl_titulo->ResumeLayout(false);
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
--- DnD_NPC_Generator/DnD_NPC_Generator/Settings.h
+++ DnD_NPC_Generator/DnD_NPC_Generator/Settings.h
@@ -34,7 +34,12 @@ namespace DnDNPCGenerator {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ lbl_Prueba;
+	private: System::Windows::Forms::Panel^ pnl_central;
+	protected:
+	private: System::Windows::Forms::Panel^ pnl_titulo;
+	private: System::Windows::Forms::Panel^ pnl_correccion;
+	private: System::Windows::Forms::Label^ lbl_titulo;
+
 	protected:
 
 	private:
@@ -50,29 +55,69 @@ namespace DnDNPCGenerator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->lbl_Prueba = (gcnew System::Windows::Forms::Label());
+			this->pnl_central = (gcnew System::Windows::Forms::Panel());
+			this->pnl_titulo = (gcnew System::Windows::Forms::Panel());
+			this->pnl_correccion = (gcnew System::Windows::Forms::Panel());
+			this->lbl_titulo = (gcnew System::Windows::Forms::Label());
+			this->pnl_titulo->SuspendLayout();
 			this->SuspendLayout();
 			// 
-			// lbl_Prueba
+			// pnl_central
 			// 
-			this->lbl_Prueba->AutoSize = true;
-			this->lbl_Prueba->Location = System::Drawing::Point(82, 124);
-			this->lbl_Prueba->Name = L"lbl_Prueba";
-			this->lbl_Prueba->Size = System::Drawing::Size(45, 13);
-			this->lbl_Prueba->TabIndex = 1;
-			this->lbl_Prueba->Text = L"Settings";
+			this->pnl_central->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(203)), static_cast<System::Int32>(static_cast<System::Byte>(228)),
+				static_cast<System::Int32>(static_cast<System::Byte>(222)));
+			this->pnl_central->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->pnl_central->Location = System::Drawing::Point(0, 90);
+			this->pnl_central->Name = L"pnl_central";
+			this->pnl_central->Size = System::Drawing::Size(1044, 571);
+			this->pnl_central->TabIndex = 5;
+			// 
+			// pnl_titulo
+			// 
+			this->pnl_titulo->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(46)), static_cast<System::Int32>(static_cast<System::Byte>(79)),
+				static_cast<System::Int32>(static_cast<System::Byte>(79)));
+			this->pnl_titulo->Controls->Add(this->lbl_titulo);
+			this->pnl_titulo->Dock = System::Windows::Forms::DockStyle::Top;
+			this->pnl_titulo->Location = System::Drawing::Point(0, 30);
+			this->pnl_titulo->Name = L"pnl_titulo";
+			this->pnl_titulo->Size = System::Drawing::Size(1044, 60);
+			this->pnl_titulo->TabIndex = 4;
+			// 
+			// pnl_correccion
+			// 
+			this->pnl_correccion->Dock = System::Windows::Forms::DockStyle::Top;
+			this->pnl_correccion->Location = System::Drawing::Point(0, 0);
+			this->pnl_correccion->Name = L"pnl_correccion";
+			this->pnl_correccion->Size = System::Drawing::Size(1044, 30);
+			this->pnl_correccion->TabIndex = 3;
+			// 
+			// lbl_titulo
+			// 
+			this->lbl_titulo->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->lbl_titulo->Font = (gcnew System::Drawing::Font(L"Algerian", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lbl_titulo->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(203)), static_cast<System::Int32>(static_cast<System::Byte>(228)),
+				static_cast<System::Int32>(static_cast<System::Byte>(222)));
+			this->lbl_titulo->Location = System::Drawing::Point(0, 0);
+			this->lbl_titulo->Name = L"lbl_titulo";
+			this->lbl_titulo->Size = System::Drawing::Size(1044, 60);
+			this->lbl_titulo->TabIndex = 0;
+			this->lbl_titulo->Text = L"Settings";
+			this->lbl_titulo->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// Settings
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1044, 661);
-			this->Controls->Add(this->lbl_Prueba);
+			this->Controls->Add(this->pnl_central);
+			this->Controls->Add(this->pnl_titulo);
+			this->Controls->Add(this->pnl_correccion);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"Settings";
 			this->Text = L"Settings";
+			this->pnl_titulo->ResumeLayout(false);
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
